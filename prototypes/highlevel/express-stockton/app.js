9f1cad9f4ad7a509eb1ba1573d72296170e984a7
stocktonsmith <a02095109@usu.edu>
2023-02-07 13:38:11 -0700

    Activity diagram complete.

--- prototypes/highlevel/express-stockton/app.js
+++ prototypes/highlevel/express-stockton/app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+let id = 0;
+const todos = [];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(todos);
 })
 
+app.post('/', (req, res) => {
+  let todo = {"id":id++, description: req.body, isCompleted: false};
+
+  todos.push(todo);
+  res.send(todo);
+}
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
