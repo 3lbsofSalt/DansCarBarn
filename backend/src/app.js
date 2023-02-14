362a2733fa9b71cf68fdd41af0543d260dfe27ac
3lbsofSalt <max.ramsdell@protonmail.com>
2023-02-14 13:14:45 -0700

    Integrating the authentication, but there's environment problems

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,16 @@
+console.log('aoirsntoiarnsteionarseiotnioaersnteionarsteionaoirsnteioanrstoein');
+import * as dotenv from 'dotenv';
+dotenv.config({ debug: true });
+
 import express from 'express';
 import router from './routes';
+import models from './models';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
