61e5921feafc4c0df89744095f9ec2a889910762
stocktonsmith <stocktonsmith24@comcast.net>
2023-02-21 11:41:51 -0700

    Merged Max's and Hyrum's login implementations

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,14 +13,6 @@ const router = createRouter({
       name: 'login',
       component: LoginView,
     },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Login.vue'),
-    },
   ],
 });
 
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -19,6 +19,36 @@
   </div>
 </template>
 
+<script>
+ export default {
+   data: () => ({
+     email: '',
+     password: ''
+   }),
+   methods: {
+     login: function() {
+       fetch('http://localhost:3001/user/login', {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+           email: this.email,
+           password: this.password
+         })
+       })
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+     }
+   }
+ }
+</script>
+
 <style scoped>
 .login-layout {
   height: 100%;
