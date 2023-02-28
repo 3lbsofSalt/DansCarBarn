6e7218559dd1362ed90e79c7c88428b3c41fe379
Hyrum Cooper <hyrumcoop@gmail.com>
2023-02-28 11:40:30 -0700

    add manage vehicles ui

---
+++ frontend/src/components/VehicleClassLabel.vue
@@ -0,0 +1,39 @@
+<script setup>
+const labelDict = {
+  gold: 'GOLD',
+  silver: 'SILVER',
+  bronze: 'BRONZE',
+};
+
+const props = defineProps(['class']);
+</script>
+
+<template>
+  <span class="class-label" :class="props.class">{{
+    labelDict[props.class]
+  }}</span>
+</template>
+
+<style scoped>
+.class-label {
+  font-size: 12px;
+  font-weight: 700;
+  padding: 6px 10px;
+  border-radius: 2px;
+}
+
+.gold {
+  background-color: #ffd700;
+  color: #aa9100;
+}
+
+.silver {
+  background-color: #c0c0c0;
+  color: #737373;
+}
+
+.bronze {
+  background-color: #cd7f32;
+  color: white;
+}
+</style>
--- frontend/src/views/manage/ManageVehiclesView.vue
+++ frontend/src/views/manage/ManageVehiclesView.vue
@@ -1,3 +1,44 @@
+<script setup>
+import ManageVehiclesCard from '../../components/ManageVehiclesCard.vue';
+
+const onDelete = () => {
+  console.log('DELETE');
+};
+</script>
+
 <template>
-  <div>Manage Vehicles Placeholder</div>
+  <div id="manage-vehicles" class="d-flex flex-column">
+    <div id="manage-users-header" class="d-flex align-center">
+      <h2 class="d-flex flex-grow-1">Vehicles</h2>
+      <v-btn icon="mdi-plus" color="secondary" size="small" />
+    </div>
+
+    <v-text-field label="Search" hide-details="auto" />
+
+    <div class="vehicles-list d-flex flex-column">
+      <ManageVehiclesCard
+        title="Ford Pinto 1972"
+        class="gold"
+        imgSrc="https://www.oldcarsweekly.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTcyODc1NjA2NTcxMDk5MzM0/1972-ford-pinto.png"
+        @delete="onDelete"
+      />
+      <ManageVehiclesCard
+        title="Ford Pinto 1972"
+        class="gold"
+        imgSrc="https://www.oldcarsweekly.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTcyODc1NjA2NTcxMDk5MzM0/1972-ford-pinto.png"
+        @delete="onDelete"
+      />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+#manage-vehicles {
+  width: 100%;
+  gap: 16px;
+}
+
+.vehicles-list {
+  gap: 8px;
+}
+</style>
