c5aacfbc234bf36cc5ebd3f42127fdf754111ed7
Hyrum Cooper <hyrumcoop@gmail.com>
2023-03-16 11:42:38 -0600

    add delete handler

--- frontend/src/views/manage/ManageVehiclesView.vue
+++ frontend/src/views/manage/ManageVehiclesView.vue
@@ -1,8 +1,23 @@
 <script setup>
 import ManageVehiclesCard from '../../components/ManageVehiclesCard.vue';
 
-const onDelete = () => {
-  console.log('DELETE');
+const vehicles = [
+  {
+    id: 1,
+    title: 'Ford Pinto 1972',
+    class: 'gold',
+    imgSrc: 'https://www.oldcarsweekly.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTcyODc1NjA2NTcxMDk5MzM0/1972-ford-pinto.png',
+  },
+  {
+    id: 2,
+    title: 'Ford Pinto 1972',
+    class: 'gold',
+    imgSrc: 'https://www.oldcarsweekly.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTcyODc1NjA2NTcxMDk5MzM0/1972-ford-pinto.png',
+  }
+];
+
+const onDelete = id => {
+  console.log('DELETE VEHICLE with ID', id);
 };
 </script>
 
@@ -17,16 +32,11 @@ const onDelete = () => {
 
     <div class="vehicles-list d-flex flex-column">
       <ManageVehiclesCard
-        title="Ford Pinto 1972"
-        class="gold"
-        imgSrc="https://www.oldcarsweekly.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTcyODc1NjA2NTcxMDk5MzM0/1972-ford-pinto.png"
-        @delete="onDelete"
-      />
-      <ManageVehiclesCard
-        title="Ford Pinto 1972"
-        class="gold"
-        imgSrc="https://www.oldcarsweekly.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTcyODc1NjA2NTcxMDk5MzM0/1972-ford-pinto.png"
-        @delete="onDelete"
+        v-for="vehicle in vehicles"
+        :title="vehicle.title"
+        :class="vehicle.class"
+        :imgSrc="vehicle.imgSrc"
+        @delete="() => onDelete(vehicle.id)"
       />
     </div>
   </div>
