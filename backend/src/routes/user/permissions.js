42a364ad4e719d77b9e4961d2667792c4f7fda22
3lbsofSalt <max.ramsdell@protonmail.com>
2023-03-23 10:51:06 -0600

    Finished the manage users page

--- backend/src/routes/user/permissions.js
+++ backend/src/routes/user/permissions.js
@@ -5,24 +5,5 @@ import safeAwait from 'safe-await';
 
 import models from '../../models';
 
-router.put('/', async (req, res) => {
-  const {
-    id,
-    role
-  } = req.body;
-
-  const [error] = await safeAwait(models.User.update({
-    role
-  },{ where: {
-    id
-  }}));
-
-  if(error) {
-    console.log(error);
-    return res.sendStatus(500);
-  }
-
-  return res.sendStatus(200);
-});
 
 export default router;
